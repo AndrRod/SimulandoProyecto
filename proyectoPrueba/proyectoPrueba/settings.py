89bd1405af389bb2e9bc9c71816229978e7586bc
Andres <rodrigueza.federacion@gmail.com>
2021-08-03 07:18:02 -0300

    configurar con base de datos en setting.py

--- proyectoPrueba/proyectoPrueba/settings.py
+++ proyectoPrueba/proyectoPrueba/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'proyectoPrueba.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Aca agregamos postgreSQL
+        'NAME': 'Registro Personas con deudas',
+        'USER': 'postgres',
+        'PASSWORD': 'Lgante03',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
