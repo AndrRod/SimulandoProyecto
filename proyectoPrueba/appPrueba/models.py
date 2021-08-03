b1890bf4e95852f1f36adb2000fa7fa717994aee
Andres <rodrigueza.federacion@gmail.com>
2021-08-03 07:28:01 -0300

    agregando clase a modulo models.py para luego migrar a base de datos con makemigrations y migrate

--- proyectoPrueba/appPrueba/models.py
+++ proyectoPrueba/appPrueba/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class deudores(models.Model):
+    nombre = models.CharField(max_length=20)
+    dni = models.IntegerField()
+    expediente = models.IntegerField()
+    def __str__(self):
+        return f'El Sr./Sra. {self.nombre}, dni: {self.dni} se encuentra afectado en {self.expediente} expedienes'
