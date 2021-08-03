d07ee3c0e52fccd05c0fa59419b6421d42f4b345
Andres <rodrigueza.federacion@gmail.com>
2021-08-03 15:59:46 -0300

    modificando archivo models.py y admin.py (para adminstrador)

--- proyectoPrueba/appPrueba/admin.py
+++ proyectoPrueba/appPrueba/admin.py
@@ -1,3 +1,10 @@
+from appPrueba.models import deudores
 from django.contrib import admin
 
+
 # Register your models here.
+
+class modelDeudores(admin.ModelAdmin):
+    list_display = ("nombre", "dni", "expediente")
+    
+admin.site.register(deudores, modelDeudores)
--- proyectoPrueba/appPrueba/models.py
+++ proyectoPrueba/appPrueba/models.py
@@ -6,6 +6,7 @@ from django.db.models.base import Model
 class deudores(models.Model):
     nombre = models.CharField(max_length=20)
     dni = models.IntegerField()
-    expediente = models.IntegerField()
+    expediente = models.IntegerField(help_text="Ingresar solo cantidad de expedientes que está afectado")
+
     def __str__(self):
         return f'El Sr./Sra. {self.nombre}, dni: {self.dni} se encuentra afectado en {self.expediente} expedienes'
