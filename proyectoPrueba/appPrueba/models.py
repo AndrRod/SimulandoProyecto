4f56d88ad504d1b05f0fb653a0f173344cf2ab90
Andres <rodrigueza.federacion@gmail.com>
2021-08-04 07:13:58 -0300

    modifique las columnas de una tabla  de models.py, como ya se había creado una tabla con datos dentro me generaba conflicto, por lo que tuve que crear columnas sin datos obligatorios (blank = True, null = True)

--- proyectoPrueba/appPrueba/models.py
+++ proyectoPrueba/appPrueba/models.py
@@ -5,8 +5,10 @@ from django.db.models.base import Model
 
 class deudores(models.Model):
     nombre = models.CharField(max_length=20)
-    dni = models.IntegerField()
+    dni = models.CharField(max_length= 8)
     expediente = models.IntegerField(help_text="Ingresar solo cantidad de expedientes que está afectado")
+    fecha_actualizacion_datos = models.DateField(blank=True, null=True)
+    regularizo_situacion = models.BooleanField(blank=True, null=True)
 
     def __str__(self):
-        return f'El Sr./Sra. {self.nombre}, dni: {self.dni} se encuentra afectado en {self.expediente} expedienes'
+        return f'El Sr./Sra. {self.nombre}, dni: {self.dni} se encuentra afectado en {self.expediente} expedienes, ultima fecha de actualización de datos: {self.fecha_actualizacion_datos}, regularizo situación: {"SI" if self.regularizo_situacion else "NO"}'
