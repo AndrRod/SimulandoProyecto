c28db78ae451b783b97363d3f9d8381c638bfff1
Andres <rodrigueza.federacion@gmail.com>
2021-08-04 07:20:28 -0300

    Esas nuevas modificaciones en models.py para que se vean de manera prolija en el panel administracion tuve que modificar admin.py agregando a la class modelDeudores los nuevos parametros

--- proyectoPrueba/appPrueba/admin.py
+++ proyectoPrueba/appPrueba/admin.py
@@ -5,6 +5,10 @@ from django.contrib import admin
 # Register your models here.
 
 class modelDeudores(admin.ModelAdmin):
-    list_display = ("nombre", "dni", "expediente")
+    list_display = ("nombre", "dni", "expediente", "fecha_actualizacion_datos", "regularizo_situacion")
+    search_fields = ("dni",)
+    list_filter=("fecha_actualizacion_datos",)
+
+
     
 admin.site.register(deudores, modelDeudores)
