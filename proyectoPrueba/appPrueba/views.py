a7f1ade64a595d630601e6da5e2b5bebead1559e
Andres <rodrigueza.federacion@gmail.com>
2021-08-04 21:27:31 -0300

    crear formulario en html (template) para concetar a traves de la vista (views.py), que a su vez tiene que estar registrada en el modulo urls.py para poder acceder a esa url

--- proyectoPrueba/appPrueba/views.py
+++ proyectoPrueba/appPrueba/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def buscar(request):
+    return render(request, "busqueda_deudor.html")
--- proyectoPrueba/proyectoPrueba/urls.py
+++ proyectoPrueba/proyectoPrueba/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appPrueba import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('buscar/', views.buscar),
 ]
