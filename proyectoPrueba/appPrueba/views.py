9dad38c4a75262c7810e420fe6ba8d6c5a83d0db
Andres <rodrigueza.federacion@gmail.com>
2021-08-05 23:37:04 -0300

    agregando condicionales a una vista

--- proyectoPrueba/appPrueba/views.py
+++ proyectoPrueba/appPrueba/views.py
@@ -18,9 +18,11 @@ def resultado_deudores(request):
        
         deudor = request.GET['dato']
 
-        if len(deudor) > 8:
-            mensaje = 'La busqueda tiene que contener menos de 8 caracteres'
+        if not deudor.isdigit():
+            mensaje = 'La busqueda tiene que contener solo numeros'
             
+        elif len(deudor) > 8 or len(deudor) < 7:
+            mensaje = 'La busqueda tiene que contener menos de 8 caracteres y más de 6 caracteres'
         
         else: 
             deudor_nombre = deudores.objects.filter(dni__icontains = deudor)
@@ -28,6 +30,6 @@ def resultado_deudores(request):
             return render(request, "resultado_deudores.html", {"dni" : deudor_nombre, "dni_dato": deudor})
     
    else:
-       mensaje = 'No has introducido nada'
+       mensaje = 'NO SE INTRODUJO NINGUN DATO'
     
    return HttpResponse(mensaje)
