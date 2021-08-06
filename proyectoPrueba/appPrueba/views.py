a65f4b4a31beff565f37f1c97b86d781c56d0771
Andres <rodrigueza.federacion@gmail.com>
2021-08-05 23:29:25 -0300

    borrando codigo que sobra

--- proyectoPrueba/appPrueba/views.py
+++ proyectoPrueba/appPrueba/views.py
@@ -12,18 +12,6 @@ def buscar(request):
 
 
 
-# def resultado_deudores(request):
-#     if request.GET['dato']:
-#         dni_dato = request.GET['dato']
-        
-#         dni = deudores.objects.filter(nombre__icontains = dni_dato)
-#         return render(request, "resultado_deudores.html", {"dni_dato" : dni_dato, "dni" : dni})
-#     else:
-#         mensaje = 'No ingreso ningún dato'
-    
-#     return HttpResponse(mensaje)
-
-
 def resultado_deudores(request):
   
    if request.GET['dato']:
@@ -33,10 +21,10 @@ def resultado_deudores(request):
         if len(deudor) > 8:
             mensaje = 'La busqueda tiene que contener menos de 8 caracteres'
             
-        # incontains sustituye o es como like, nombre es lo que haya en deudor
+        
         else: 
             deudor_nombre = deudores.objects.filter(dni__icontains = deudor)
-            #    redirigir Art a otro archivo html
+        
             return render(request, "resultado_deudores.html", {"dni" : deudor_nombre, "dni_dato": deudor})
     
    else:
