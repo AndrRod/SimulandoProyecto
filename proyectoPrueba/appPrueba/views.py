76166013a279e042c873c54ac9f96b2c12923f87
Andres <rodrigueza.federacion@gmail.com>
2021-08-05 23:23:47 -0300

    Encontre el error en el formulario del html (busqueda_deudor) en la parte de action coloque que redirija al mismo html (busqueda_deudor) y no al html donde envie la informacion captada por el metodo GET (resultado_deudores.html)

--- proyectoPrueba/appPrueba/views.py
+++ proyectoPrueba/appPrueba/views.py
@@ -8,16 +8,38 @@ from appPrueba.models import deudores
 # Create your views here.
 
 def buscar(request):
-    return render(request, "busqueda_deudor.html")
+    return render(request, 'busqueda_deudor.html')
+
+
+
+# def resultado_deudores(request):
+#     if request.GET['dato']:
+#         dni_dato = request.GET['dato']
+        
+#         dni = deudores.objects.filter(nombre__icontains = dni_dato)
+#         return render(request, "resultado_deudores.html", {"dni_dato" : dni_dato, "dni" : dni})
+#     else:
+#         mensaje = 'No ingreso ningún dato'
+    
+#     return HttpResponse(mensaje)
 
 
 def resultado_deudores(request):
-    if request.GET['dato']:
-        dni_dato = request.GET['dato']
-        
-        dni = deudores.objects.filter(nombre__icontains = dni_dato)
-        return render(request, "resultado_deudores.html", {"dni_dato" : dni_dato, "dni" : dni})
-    else:
-        mensaje = 'No ingreso ningún dato'
+  
+   if request.GET['dato']:
+       
+        deudor = request.GET['dato']
+
+        if len(deudor) > 8:
+            mensaje = 'La busqueda tiene que contener menos de 8 caracteres'
+            
+        # incontains sustituye o es como like, nombre es lo que haya en deudor
+        else: 
+            deudor_nombre = deudores.objects.filter(dni__icontains = deudor)
+            #    redirigir Art a otro archivo html
+            return render(request, "resultado_deudores.html", {"dni" : deudor_nombre, "dni_dato": deudor})
     
-    return HttpResponse(mensaje)
+   else:
+       mensaje = 'No has introducido nada'
+    
+   return HttpResponse(mensaje)
